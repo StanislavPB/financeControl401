f91d8b0729bac03ece039415011af42d0afafe4e
StanislavPB <perminovsb@gmail.com>
2024-03-18 21:31:18 +0000

    Initial commit

---
+++ src/main/java/org/financecontrol401/FinanceControl401Application.java
@@ -0,0 +1,13 @@
+package org.financecontrol401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControl401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceControl401Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/financecontrol401/FinanceControl401ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.financecontrol401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControl401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
