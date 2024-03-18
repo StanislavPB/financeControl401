62e042638ac78d874dd967a54289f3619e6573d8
StanislavPB <perminovsb@gmail.com>
2024-03-18 21:31:31 +0000

    Initial commit

--- src/main/java/org/financecontrol401/FinanceControl401Application.java
+++ src/main/java/org/financecontrol401/FinanceControl401Application.java
@@ -1,13 +1,10 @@
 package org.financecontrol401;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class FinanceControl401Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceControl401Application.class, args);
-	}
+    public static void main(String[] args) {
+
+         }
 
 }
