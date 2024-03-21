9c675d6dd28a4eb0bd6c6e9b58064f5bfc35b192
StanislavPB <perminovsb@gmail.com>
2024-03-21 12:16:28 +0000

    services

--- src/main/java/org/financecontrol401/service/ServiceFind.java
+++ src/main/java/org/financecontrol401/service/ServiceFind.java
@@ -1,5 +1,31 @@
 package org.financecontrol401.service;
 
-public class ServiceFind {
+import org.financecontrol401.entity.Transaction;
+import org.financecontrol401.repository.TransactionRepository;
 
+import java.util.List;
+
+// Сервис для поиска транзакций
+public class ServiceFind {
+    private final TransactionRepository transactionRepository;
+
+    public ServiceFind(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    public List<Transaction> findByDate(String date) {
+        return transactionRepository.findByDate(date);
+    }
+
+    public List<Transaction> findByCategory(String category) {
+        return transactionRepository.findByCategory(category);
+    }
+
+    public List<Transaction> findByDateAndCategory(String date, String category) {
+        return transactionRepository.findByDateAndCategory(date, category);
+    }
 }
--- src/main/java/org/financecontrol401/service/ServicePrint.java
+++ src/main/java/org/financecontrol401/service/ServicePrint.java
@@ -1,4 +1,21 @@
 package org.financecontrol401.service;
 
+import java.util.List;
+import org.financecontrol401.entity.Transaction;
+
+// Сервис для печати информации о транзакциях
 public class ServicePrint {
+    public void printAll(List<Transaction> transactions) {
+        for (Transaction transaction : transactions) {
+            System.out.println(transaction);
+        }
+    }
+
+    public void printByDate(List<Transaction> transactions) {
+        // TODO: Реализация печати по дате
+    }
+
+    public void printByCategory(List<Transaction> transactions) {
+        // TODO: Реализация печати по категории
+    }
 }
