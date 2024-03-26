2f736dc27e1ec8efb4ad16cf357b750858a1aaf7
vtarlovsky <vzvagolsky@gmai.com>
2024-03-26 01:26:45 +0100

    Измения

--- src/main/java/org/financecontrol401/Main.java
+++ src/main/java/org/financecontrol401/Main.java
@@ -1,14 +1,17 @@
 package org.financecontrol401;
 
+import org.financecontrol401.dto.ClientRequestTransaction;
+import org.financecontrol401.entity.Transaction;
+import org.financecontrol401.entity.TransactionType;
 import org.financecontrol401.repository.CategoryRepository;
 import org.financecontrol401.repository.TransactionRepository;
-import org.financecontrol401.service.BalanceService;
-import org.financecontrol401.service.CategoryService;
+import org.financecontrol401.service.*;
 
-import org.financecontrol401.service.Menu;
-import org.financecontrol401.service.TransactionService;
+import org.financecontrol401.ui.InputReader;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,12 +35,40 @@ public class Main {
 
 
 
+//Ввод--------------------------------------
+            ClientRequestTransaction requestTransaction = InputReader.readTransaction();
+
+            transactionService.addTransaction(requestTransaction);
+
+
+//===================================
+
+            //Вывод
+
+
+            FindService findService=new FindService(transactionRepository);
+
+            List<Transaction> findetlist= findService.findAll();
+            //System.out.println(findetlist);
+
+            LocalDate startDate = LocalDate.of(2023, 1, 1);
+            LocalDate endDate = LocalDate.of(2024, 4, 1);
+            TransactionType type = TransactionType.INCOME;
+
+            // Вызываем метод findByTypeAndPeriod и получаем список транзакций
+            List<Transaction> findetlist2 =
+                    findService.findByTypeAndPeriod(startDate, endDate, type);
+
+            // Выводим найденные транзакции
+            System.out.println("Найденные транзакции:");
+            for (Transaction transaction : findetlist2) {
+                System.out.println(transaction); // Предполагается, что у класса Transaction переопределен метод toString()
+            }
 
 
 
 
-
-
+/*
 
             Menu menu = new Menu(categoryservice);
 
@@ -46,7 +77,7 @@ public class Main {
 
             menu.displayCategoryMenu(operationType);
             int categoryChoice = menu.getUserChoice();
-
+*/
 
 
         } catch (IOException e) {
